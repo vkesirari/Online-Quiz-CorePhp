9b70dcfe1448f719142743d0cd1e3aae80daa043
vikramjeet <vikkysirari143@gmail.com>
2020-06-04 12:00:03 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Online-Quiz-System
+Today Online Quiz System has become a fast growing examination method because of its speed and accuracy. It is also needed less manpower to execute the examination. Almost all organizations now-a-days, are conducting their objective exams by online examination or quiz system, it saves students time in examinations. Organizations can also easily check the performance of the student that they give in an examination. As a result of this, organizations are releasing results in less time. It also helps the environment by saving paper. According to today’s requirement, online examination project in PHP is very useful to learn it.
+
+# To execute this file
+
+=> Download wamp or xamp and run it.
+=> for wamp user copy online-quiz folder to c/wamp/www 
+and for xamp user copy this to c/xamp/htdocs
+=> after then open your browser type localhost/phpmyadmin 
+then enter your username root and left password black and enter.
+Then you will see databases,
+create new database ,name it exam and import exam.sql file from online-quiz folder.
+and now open new tab and write localhost/online-quiz
+and register it and start your Quiz..
+
+=> for admin access
+ localhost/online-quiz/admin
+ username = nisha@gmail.com
+ password nisha123
---
+++ exam.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 27, 2020 at 05:13 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'nisha@gmail.com', 'nisha123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE IF NOT EXISTS `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('5b13ed3a6e006', '5b13ed3a9436a'),
+('5b13ed72489d8', '5b13ed7263d70'),
+('5b141d712647f', '5b141d71485b9'),
+('5b141d718f873', '5b141d71978be'),
+('5b141d71ddb46', '5b141d71e5f43'),
+('5b141d721a738', '5b141d7222884'),
+('5b141d7260b7d', '5b141d7268b9a'),
+('5b141d72a6fa1', '5b141d72aefcb'),
+('5b141d72d7a1c', '5b141d72dfa7b'),
+('5b141d731429b', '5b141d731c234'),
+('5b141d7345176', '5b141d734cd1b'),
+('5b141d737ddfc', '5b141d73858df');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE IF NOT EXISTS `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE IF NOT EXISTS `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('5b13ed3a6e006', 'sdb', '5b13ed3a9436a'),
+('5b13ed3a6e006', 'jsdb', '5b13ed3a94374'),
+('5b13ed3a6e006', 'dsbv', '5b13ed3a94377'),
+('5b13ed3a6e006', 'jbdv', '5b13ed3a94379'),
+('5b13ed72489d8', 'vsdv', '5b13ed7263d70'),
+('5b13ed72489d8', 'vsdv', '5b13ed7263d7a'),
+('5b13ed72489d8', 'vsdv', '5b13ed7263d7d'),
+('5b13ed72489d8', 'vsdv', '5b13ed7263d80'),
+('5b141d712647f', 'Personal Home Page', '5b141d71485b9'),
+('5b141d712647f', 'Private Home Page', '5b141d71485dc'),
+('5b141d712647f', 'Pretext Hypertext Processor', '5b141d71485e0'),
+('5b141d712647f', 'Preprocessor Home Page', '5b141d71485e4'),
+('5b141d718f873', 'Rasmus Lerdorf', '5b141d71978be'),
+('5b141d718f873', 'Willam Makepiece', '5b141d71978cc'),
+('5b141d718f873', 'Drek Kolkevi', '5b141d71978d1'),
+('5b141d718f873', 'List Barely', '5b141d71978d4'),
+('5b141d71ddb46', '.html', '5b141d71e5f2b'),
+('5b141d71ddb46', '.ph', '5b141d71e5f3c'),
+('5b141d71ddb46', '.php', '5b141d71e5f43'),
+('5b141d71ddb46', '.xml', '5b141d71e5f48'),
+('5b141d721a738', 'for loop', '5b141d7222820'),
+('5b141d721a738', 'do-while loop', '5b141d722282f'),
+('5b141d721a738', 'foreach loop', '5b141d7222880'),
+('5b141d721a738', 'All of the above', '5b141d7222884'),
+('5b141d7260b7d', 'echo (â€œHello Worldâ€);', '5b141d7268b8a'),
+('5b141d7260b7d', 'print (â€œHello Worldâ€);', '5b141d7268b95'),
+('5b141d7260b7d', 'printf (â€œHello Worldâ€);', '5b141d7268b98'),
+('5b141d7260b7d', 'All of the above', '5b141d7268b9a'),
+('5b141d72a6fa1', 'file()', '5b141d72aefcb'),
+('5b141d72a6fa1', 'arr_file()', '5b141d72aefd8'),
+('5b141d72a6fa1', 'arrfile()', '5b141d72aefdc'),
+('5b141d72a6fa1', 'file_arr()', '5b141d72aefe0'),
+('5b141d72d7a1c', 'Magic Function', '5b141d72dfa7b'),
+('5b141d72d7a1c', 'Inbuilt Function', '5b141d72dfa85'),
+('5b141d72d7a1c', 'Default Function', '5b141d72dfa88'),
+('5b141d72d7a1c', 'User Defined Function', '5b141d72dfa8b'),
+('5b141d731429b', 'CREATE TABLE table_name (column_name column_type);', '5b141d731c234'),
+('5b141d731429b', 'CREATE table_name (column_type column_name);', '5b141d731c242'),
+('5b141d731429b', 'CREATE table_name (column_name column_type);', '5b141d731c248'),
+('5b141d731429b', 'CREATE TABLE table_name (column_type column_name);', '5b141d731c24b'),
+('5b141d7345176', 'get_array() and get_row()', '5b141d734cd10'),
+('5b141d7345176', 'fetch_array() and fetch_row()', '5b141d734cd1b'),
+('5b141d7345176', 'get_array() and get_column()', '5b141d734cd1d'),
+('5b141d7345176', 'fetch_array() and fetch_column()', '5b141d734cd20'),
+('5b141d737ddfc', 'explode()', '5b141d73858d0'),
+('5b141d737ddfc', 'implode()', '5b141d73858df'),
+('5b141d737ddfc', 'concat()', '5b141d73858e3'),
+('5b141d737ddfc', 'concatenate()', '5b141d73858e8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE IF NOT EXISTS `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('5b141b8009cf0', '5b141d712647f', 'What does PHP stand for?', 4, 1),
+('5b141b8009cf0', '5b141d718f873', 'Who is the father of PHP?', 4, 2),
+('5b141b8009cf0', '5b141d71ddb46', 'PHP files have a default file extension of.', 4, 3),
+('5b141b8009cf0', '5b141d721a738', 'Which of the looping statements is/are supported by PHP?', 4, 4),
+('5b141b8009cf0', '5b141d7260b7d', 'Which of the following PHP statements will output Hello World on the screen?', 4, 5),
+('5b141b8009cf0', '5b141d72a6fa1', 'Which one of the following function is capable of reading a file into an array?', 4, 6),
+('5b141b8009cf0', '5b141d72d7a1c', 'A function in PHP which starts with __ (double underscore) is know as..', 4, 7),
+('5b141b8009cf0', '5b141d731429b', 'Which one of the following statements is used to create a table?', 4, 8),
+('5b141b8009cf0', '5b141d7345176', 'Which of the methods are used to manage result sets using both associative and indexed arrays?', 4, 9),
+('5b141b8009cf0', '5b141d737ddfc', 'Which one of the following functions can be used to concatenate array elements to form a single delimited string?', 4, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+CREATE TABLE IF NOT EXISTS `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `date`) VALUES
+('5b141b8009cf0', 'Php & Mysqli', 3, 1, 10, '2018-06-03 16:46:56'),
+('5ecdeb98761c3', 'Database', 2, 1, 5, '2020-05-27 04:24:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+DROP TABLE IF EXISTS `rank`;
+CREATE TABLE IF NOT EXISTS `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('nishu@test.com', -4, '2020-05-27 04:30:35'),
+('nisha@test.com', 3, '2020-05-27 04:32:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `name` varchar(50) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `college`, `email`, `password`) VALUES
+('rawat', 'aa', 'nisha@test.com', 'nisha@123'),
+('negi', 'bb', 'nishu@test.com', 'rx@123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
